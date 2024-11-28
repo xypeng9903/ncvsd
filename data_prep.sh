6e3f6d1aba4200848073cb6566a2dcba186feed6
pengxinyu <xypeng9903@outlook.com>
2024-11-28 18:49:37 +0800

    update data_prep

--- data_prep.sh
+++ data_prep.sh
@@ -1,11 +1,9 @@
 # python dataset_tool.py convert \
-#         --source=/sharedata/home/pxy/workspace/data/train/ILSVRC/Data/CLS-LOC/train \
-#         --dest=/sharedata/home/pxy/workspace/data/train/edm/edm2-imagenet-64x64.zip \
-#         --resolution=64x64 \
+#         --source=/data0/pxy/ILSVRC/Data/CLS-LOC/train \
+#         --dest=/data0/pxy/code/data/train/edm/img512.zip \
+#         --resolution=512x512 \
 #         --transform=center-crop-dhariwal
 
-python dataset_tool.py convert \
-        --source=/data0/pxy/ILSVRC/Data/CLS-LOC/train \
-        --dest=/data0/pxy/code/data/train/edm/img512.zip \
-        --resolution=512x512 \
-        --transform=center-crop-dhariwal
+python dataset_tool.py encode \
+        --source=/data0/pxy/code/data/train/edm/img512.zip \
+        --dest=/data0/pxy/code/data/train/edm/img512-sd.zip
