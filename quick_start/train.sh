fc9397d533d7a1b8adb4b9701f7df58eadb5c621
pengxinyu <xypeng9903@outlook.com>
2024-12-02 17:11:34 +0800

    add img64-xl

--- quick_start/train.sh
+++ quick_start/train.sh
@@ -1,11 +1,26 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
+
+#---------------------------------------------------------
+# batch size 128
 
 # img64-s (bsz 128)
+# torchrun --standalone --nproc_per_node=1 train_edm2.py \
+#     --outdir="training-runs/img64-s-bsz-128" \
+#     --preset="presets/img64-s-bsz-128.json" \
+#     --data="../data/train/edm/img64.zip" \
+#     --net="../model_zoo/edm2/imagenet64-s.pkl" \
+#     --batch-gpu=8 \
+#     --batch=128 \
+#     --duration="16Mi" \
+#     --checkpoint="1Mi" \
+#     --snapshot="1Mi" \
+#     --grad-checkpoint=False
+
 torchrun --standalone --nproc_per_node=1 train_edm2.py \
-    --outdir="training-runs/img64-s-bsz-128" \
-    --preset="presets/img64-s-bsz-128.json" \
+    --outdir="training-runs/img64-xl-bsz-128" \
+    --preset="presets/img64-xl-bsz-128.json" \
     --data="../data/train/edm/img64.zip" \
-    --net="../model_zoo/edm2/imagenet64-s.pkl" \
+    --net="../model_zoo/edm2/edm2-img64-xl-0671088-0.040.pkl" \
     --batch-gpu=8 \
     --batch=128 \
     --duration="16Mi" \
@@ -13,7 +28,24 @@ torchrun --standalone --nproc_per_node=1 train_edm2.py \
     --snapshot="1Mi" \
     --grad-checkpoint=False
 
+
+#---------------------------------------------------------
+# batch size 2048
+
 # img64-s
+# torchrun --standalone --nproc_per_node=1 train_edm2.py \
+#     --net="../model_zoo/edm2/imagenet64-s.pkl" \
+#     --outdir="training-runs/img64-s" \
+#     --data="../data/train/edm/img64.zip" \
+#     --preset="presets/img64-s.json" \
+#     --batch-gpu=8 \
+#     --batch=2048 \
+#     --duration="128Mi" \
+#     --checkpoint="1Mi" \
+#     --snapshot="1Mi" \
+#     --grad-checkpoint=False
+
+# img64-xl
 # torchrun --standalone --nproc_per_node=1 train_edm2.py \
 #     --net="../model_zoo/edm2/imagenet64-s.pkl" \
 #     --outdir="training-runs/img64-s" \
