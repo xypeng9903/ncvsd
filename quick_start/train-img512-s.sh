acc690a6f82f831ebff15653f6809163ba3ef3c0
xinyu_peng <xypeng9903@outlook.com>
2024-12-14 12:25:39 +0000

    update

---
+++ quick_start/train-img512-s.sh
@@ -0,0 +1,14 @@
+NUM_GPUS=$1
+BATCH_GPU=$2
+
+
+torchrun --standalone --nproc_per_node=$NUM_GPUS train_edm2.py \
+    --preset="presets/img512-s-v1.json" \
+    --outdir="training-runs/dev-img512-s-v1" \
+    --net="../model_zoo/edm2/edm2-img512-s-2147483-0.130.pkl" \
+    --data="../data/edm2/img512-sd.zip" \
+    --batch-gpu=$BATCH_GPU \
+    --batch=128 \
+    --duration="16Mi" \
+    --checkpoint="1Mi" \
+    --snapshot="128Ki"
