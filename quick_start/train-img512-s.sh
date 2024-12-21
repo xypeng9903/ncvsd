27079f267f04a7f69e7cbd76341327e3cb763bc5
xinyu_peng <xypeng9903@outlook.com>
2024-12-21 17:48:11 +0800

    add eval_ts

--- quick_start/train-img512-s.sh
+++ quick_start/train-img512-s.sh
@@ -7,6 +7,7 @@ export HF_ENDPOINT="http://hf-mirror.com"
 torchrun --standalone --nproc_per_node=$NUM_GPUS train_edm2.py \
     --preset="presets/img512-s.json" \
     --outdir="training-runs/dev-img512-s" \
+    --ts="10,22,39" \
     --net="../model_zoo/edm2/edm2-img512-s-2147483-0.130.pkl" \
     --data="../data/edm2/img512-sd.zip" \
     --batch-gpu=$BATCH_GPU \
