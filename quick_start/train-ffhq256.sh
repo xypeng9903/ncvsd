3338572d1266e886f6ba846cfe06ccb335890376
xinyu_peng <xypeng9903@outlook.com>
2024-12-24 15:50:03 +0800

    update

--- quick_start/train-ffhq256.sh
+++ quick_start/train-ffhq256.sh
@@ -3,8 +3,8 @@ BATCH_GPU=$2
 
 
 torchrun --standalone --nproc_per_node=$NUM_GPUS train_edm2.py \
-    --preset="presets/ffhq256-v1.json" \
-    --outdir="training-runs/ffhq256-add-gan-loss-v1" \
+    --preset="presets/ffhq256.json" \
+    --outdir="training-runs/ffhq256" \
     --ts="10,22,39" \
     --batch-gpu=$BATCH_GPU \
     --duration="16Mi" \
