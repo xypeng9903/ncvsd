233328fa4324dd32b9679c74bf5fcf1d16c3ceb6
pengxinyu <xypeng9903@outlook.com>
2024-12-28 10:44:50 +0800

    update quick_start

--- quick_start/eval-img64.sh
+++ quick_start/eval-img64.sh
@@ -1,7 +1,9 @@
 NUM_GPUS=$1
 NET=$2
+TS=$3
 
 torchrun --standalone --nproc_per_node=$NUM_GPUS calculate_metrics.py gen \
         --net=$NET \
+        --ts=$TS \
         --ref="https://nvlabs-fi-cdn.nvidia.com/edm2/dataset-refs/img64.pkl" \
         --seed=123456789
