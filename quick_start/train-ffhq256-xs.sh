0f31a96a6dfedc6a78ebbf786cdf824a5300b2b9
pengxinyu <xypeng9903@outlook.com>
2025-01-02 12:52:14 +0800

    add ffhq256-xs

---
+++ quick_start/train-ffhq256-xs.sh
@@ -0,0 +1,14 @@
+NUM_GPUS=$1
+BATCH_GPU=$2
+
+torchrun --standalone --nproc_per_node=$NUM_GPUS train_edm2.py \
+    --preset="presets/ffhq256-xs.json" \
+    --outdir="training-runs/ffhq256-xs" \
+    --net="../model_zoo/edm2/edm2-ffhq256.pkl" \
+    --data="../data/edm2/ffhq256.zip" \
+    --batch-gpu=$BATCH_GPU \
+    --batch=256 \
+    --duration="4Mi" \
+    --checkpoint="128Ki" \
+    --snapshot="128Ki" \
+    --ts="10,22,39"
