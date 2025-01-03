ddf408f88a0d994bf164b7fa6c840da85848ecfb
pengxinyu <xypeng9903@outlook.com>
2025-01-03 20:46:19 +0800

    update presets

--- quick_start/train-ffhq256-s.sh
+++ quick_start/train-ffhq256-s.sh
@@ -2,8 +2,8 @@ NUM_GPUS=$1
 BATCH_GPU=$2
 
 torchrun --standalone --nproc_per_node=$NUM_GPUS train_edm2.py \
-    --preset="edm2-ffhq256-xs" \
-    --outdir="training-runs/edm2-ffhq256-xs" \
+    --preset="edm2-ffhq256-s" \
+    --outdir="training-runs/edm2-ffhq256-s" \
     --data="../data/edm2/ffhq256.zip" \
     --batch-gpu=$BATCH_GPU \
     --cond=False \
