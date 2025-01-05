77a137d43777c1436286fda3e7241d2425199dae
pengxinyu <xypeng9903@outlook.com>
2025-01-05 13:36:14 +0800

    update hyparams

--- quick_start/posterior-sample.sh
+++ quick_start/posterior-sample.sh
@@ -4,7 +4,7 @@ export HF_HOME="../model_zoo/huggingface"
 export HF_ENDPOINT="http://hf-mirror.com"
 
 python posterior_sample.py latent \
-    --net="training-runs/img512-s-uncond/snapshot-0005767-0.050.pkl" \
+    --net="training-runs/img512-s-uncond/snapshot-0011010-0.050.pkl" \
     --data="../data/test/ffhq_val" \
     --preset="presets/task/$TASK.yaml" \
     --outdir="out/$TASK" \
