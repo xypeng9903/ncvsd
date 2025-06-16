8e4bfec1b95f22eb4158f5a8252f93eb4904263e
Xinyu Peng <88471492+xypeng9903@users.noreply.github.com>
2025-06-17 00:11:10 +0800

    Update train-ffhq256-xs.sh

--- quick_start/train-ffhq256-xs.sh
+++ quick_start/train-ffhq256-xs.sh
@@ -3,7 +3,7 @@ DISTRIBUTED_ARGS="--standalone --nproc_per_node=8"
 torchrun $DISTRIBUTED_ARGS train_edm2.py \
     --preset="presets/ffhq256-xs.json" \
     --outdir="training-runs/ffhq256-xs" \
-    --net="./model_zoo/edm2/edm2-ffhq256-xs.pkl" \
+    --net="../model_zoo/edm2/edm2-ffhq256-xs.pkl" \
     --data="../data/edm2/ffhq256.zip" \
     --cond=False \
     --batch-gpu=16 \
